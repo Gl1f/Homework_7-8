b694d271e86531ab6dfa20e927c43392c5648b3b
Gl1f <k.max2517@gmail.com>
2025-03-05 15:01:48 +0300

    Добавление датакласса City, добавление сохранение данных о городах, без аннотаций и документации

--- Game_of_city.py
+++ Game_of_city.py
@@ -1,4 +1,17 @@
 import json
+from dataclasses import dataclass
+
+
+@dataclass
+class City:
+    name: str
+    population: int
+    subject: str
+    district: str
+    latitude: float
+    longitude: float
+    is_used: bool = False
+
 
 class JsonFile:
     def __init__(self, file_path):
@@ -10,6 +23,21 @@ class JsonFile:
         return data
     
     def write_data(self, data):
-        with open(self.file_path, 'a') as file:
+        with open(self.file_path, 'w') as file:
             json.dump(data, file)
 
+class CitiesSerializer:
+    def __init__(self, city_data):
+        self.cities = [City(
+            name=city['name'],
+            population=city['population'],
+            subject=city['subject'],
+            district=city['district'],
+            latitude=city['latitude'],
+            longitude=city['longitude'],
+            is_used=city['is_used']
+        ) for city in city_data]
+        
+    def get_all_cities(self):
+        return self.cities
+    
